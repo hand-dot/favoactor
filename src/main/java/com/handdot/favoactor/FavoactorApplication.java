78e0f5f6804dcb2374b7fff6eb9436d433820012
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-05-29 02:15:23 +0900

    first commit

---
+++ src/main/java/com/handdot/favoactor/FavoactorApplication.java
@@ -0,0 +1,12 @@
+package com.handdot.favoactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoactorApplication.class, args);
+	}
+}
