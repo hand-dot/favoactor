6b47aa50ac66911f33c3b4025dad43bebb789ae7
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-06-12 00:18:58 +0900

    OAuth認証処理を追加

--- src/main/java/com/handdot/favoactor/controller/MainController.java
+++ src/main/java/com/handdot/favoactor/controller/MainController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.handdot.favoactor.service.TwitterService;
 
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 
 @Controller
 @RequestMapping("/")
@@ -18,15 +21,48 @@ public class MainController {
 	@Autowired
 	TwitterService twitterService;
 
-	@RequestMapping(params="oauth_token", method=RequestMethod.GET)
-	String login(@RequestParam String oauth_token, Model model){
-		model.addAttribute("oauth_token", oauth_token);
-		return "top";
-	}
+	private Twitter twitter;
 
-	@RequestMapping(value="login",method=RequestMethod.GET)
-	String login(Model model) throws TwitterException{
-		model.addAttribute("authorizationURL", twitterService.getAuthorizationURL());
-		return "login";
+	private RequestToken requestToken;
+
+	private AccessToken accessToken;
+
+	/**
+	 * ルートにマッピングされたメソッド
+	 * ツイッター認証が行われていない場合、認証をさせる
+	 * 認証がされている場合はトークンを保存し、処理をする
+	 * @param oauth_token
+	 * @param oauth_verifier
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	String login(@RequestParam(value = "oauth_token", required = false) String oauth_token,
+			@RequestParam(value = "oauth_verifier", required = false) String oauth_verifier, Model model) {
+		try {
+			if (oauth_token == null || oauth_verifier == null || twitter == null || requestToken == null) {
+				// 認証がまだ済んでいない場合
+				twitter = twitterService.createNewTwitter();
+				requestToken = twitter.getOAuthRequestToken();
+				model.addAttribute("authorizationURL", requestToken.getAuthorizationURL());
+				return "login";
+			} else {
+				// すでに認証がされている場合
+				if(accessToken == null){
+					accessToken = twitter.getOAuthAccessToken(requestToken, oauth_verifier);
+				}
+				//ここからOAuthつかった処理をスタート
+
+
+				model.addAttribute("oauth_verifier", oauth_verifier);
+				model.addAttribute("oauth_token", oauth_token);
+			}
+		} catch (TwitterException e) {
+			// 認証で失敗した場合、ツイッターインスタンスをもう一度作り直し、
+			// 再度ログイン処理を行わせる
+			twitter = null;
+			requestToken = null;
+		}
+		return "top";
 	}
 }
--- src/main/java/com/handdot/favoactor/service/TwitterService.java
+++ src/main/java/com/handdot/favoactor/service/TwitterService.java
@@ -1,76 +1,30 @@
 package com.handdot.favoactor.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import twitter4j.Status;
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.User;
-import twitter4j.auth.RequestToken;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
-/**
- * TwitterのAPIサービス
- *
- * @author hand-dot
- *
- */
 @Service
 public class TwitterService {
-
 	@Value("${oauth.consumerKey}")
 	private String consumerKey;
 
 	@Value("${oauth.consumerSecret}")
 	private String consumerSecret;
 
-
-	private Twitter twitter;
-
 	/**
-	 * 認証をするためのURLを生成します。
-	 *
-	 * @return 認証用のURL
-	 * @throws TwitterException
+	 * 新しいインスタンスを作成します。
+	 * @return ツイッターインスタンス
 	 */
-	public String getAuthorizationURL() throws TwitterException {
+	public Twitter createNewTwitter() {
 		ConfigurationBuilder builder = new ConfigurationBuilder();
 		builder.setOAuthConsumerKey(consumerKey);
 		builder.setOAuthConsumerSecret(consumerSecret);
 		Configuration configuration = builder.build();
-		TwitterFactory factory = new TwitterFactory(configuration);
-
-		twitter = factory.getInstance();
-		RequestToken requestToken = twitter.getOAuthRequestToken();
-
-		return requestToken.getAuthorizationURL();
-	}
-
-	/**
-	 * 引数に渡したユーザーのタイムラインを取得します。
-	 *
-	 * @param user
-	 * @return
-	 * @throws TwitterException
-	 */
-	public List<Status> getUserTimeline(User user) throws TwitterException {
-		return twitter.getUserTimeline(user.getId());
-	}
-
-	/**
-	 * スクリーンネームからユーザーを取得します。
-	 *
-	 * @param screenName
-	 *            スクリーンネーム
-	 * @return ツイッターユーザー
-	 * @throws TwitterException
-	 */
-	public User getUserFromScreenName(String screenName) throws TwitterException {
-		return twitter.showUser(screenName);
+		return new TwitterFactory(configuration).getInstance();
 	}
 }
